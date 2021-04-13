c9a852388fe18ca6cfb139fe444dbc1a2f8045d3
Ferran Rosales <ferranrosales@mac.com>
2021-04-13 15:15:03 +0200

    Initial Commit

---
+++ ML Count Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ML Count Words
+//
+//  Created by Ferran Rosales on 13/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
