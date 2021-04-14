b96de7645f0690e3d6b75282d79395f6e8875f9b
Ferran Rosales <ferranrosales@mac.com>
2021-04-14 13:12:50 +0200

    Initial commit and create Home Screen

---
+++ ML Count Words/Home/Model/CustomTableViewCell.swift
@@ -0,0 +1,94 @@
+//
+//  CustomTableViewCell.swift
+//  ML Count Words
+//
+//  Created by Ferran Rosales on 14/4/21.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    let imageCell: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "file_icon")
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 34).isActive = true
+        return imageView
+    }()
+    
+    let chevron: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "chevron_right")
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.heightAnchor.constraint(equalToConstant: 22).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 12).isActive = true
+        return imageView
+    }()
+    
+    let filename: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.textColor = UIColor(named: "text_azul")
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "File name"
+
+        return label
+    }()
+    let numberOfWords: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textColor = UIColor(named: "text_azul")
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "125 palabras"
+
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.backgroundColor = UIColor(named: "back_light_green")
+        //contentView.heightAnchor.constraint(equalToConstant: 62).isActive = true
+        
+        contentView.addSubview(imageCell)
+        imageCell.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8).isActive = true
+        imageCell.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
+        
+        contentView.addSubview(filename)
+        filename.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12).isActive = true
+        filename.leadingAnchor.constraint(equalTo: imageCell.trailingAnchor, constant: 16).isActive = true
+        
+        contentView.addSubview(numberOfWords)
+        numberOfWords.topAnchor.constraint(equalTo: filename.bottomAnchor, constant: 7).isActive = true
+        numberOfWords.leadingAnchor.constraint(equalTo: filename.leadingAnchor).isActive = true
+        
+        contentView.addSubview(chevron)
+        chevron.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        chevron.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16).isActive = true
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0))
+        layer.cornerRadius = 15
+        layer.masksToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ML Count Words/Home/UI/ViewController.swift
@@ -0,0 +1,228 @@
+//
+//  HomeViewController.swift
+//  ML Count Words
+//
+//  Created by Ferran Rosales on 13/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let contentView = UIStackView()
+    
+    let header = UIView()
+    let recents = UIView()
+    
+    let tableview = UITableView()
+    
+    var fakeData = ["Uno", "Dos", "Tres"]
+    
+    let headerImageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.image = UIImage(named: "home_header")
+        imageview.contentMode = .scaleToFill
+
+        imageview.translatesAutoresizingMaskIntoConstraints = false
+        return imageview
+    }()
+    
+    let homeImageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.image = UIImage(named: "home_image")
+        imageview.contentMode = .scaleToFill
+        imageview.translatesAutoresizingMaskIntoConstraints = false
+        imageview.heightAnchor.constraint(equalToConstant: 128).isActive = true
+        imageview.widthAnchor.constraint(equalToConstant: 111).isActive = true
+        return imageview
+    }()
+    
+    let menuButton: UIButton = {
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 20))
+        let image = UIImage(named: "Menu Icon")
+        button.setImage(image , for: .normal)
+        button.isUserInteractionEnabled = true
+        button.addTarget(self, action: #selector(menu(sender:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 17).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 26).isActive = true
+        
+        return button
+        
+    }()
+    
+    let bienvenido: UILabel = {
+        
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 36, weight: .bold)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Bienvenido"
+
+        return label
+        
+    }()
+    
+    let bienvenidoSub: UILabel = {
+        
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Encuentra y cuenta palabras"
+
+        return label
+        
+    }()
+    
+    let archivosRecientes: UILabel = {
+        
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = UIColor(named: "text_azul")
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Archivos Recientes"
+
+        return label
+        
+    }()
+    
+    let loadNewFileButton: UIButton = {
+        
+        let button = UIButton()
+        button.backgroundColor = UIColor(named: "text_azul")
+        button.setTitle("Cargar nuevo archivo", for: .normal)
+        button.titleLabel?.textColor = .white
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        button.layer.cornerRadius = 8
+        button.isUserInteractionEnabled = true
+        button.addTarget(self, action: #selector(menu(sender:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        return button
+        
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        setupContentView()
+        setupHeader()
+        setupRecents()
+        setupTableView()
+    }
+    
+    
+    
+    
+    private func setupContentView() {
+        contentView.axis = .vertical
+        contentView.distribution = .fill
+        contentView.alignment = .fill
+        //contentView.spacing = 10
+        
+        view.addSubview(contentView)
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        }
+    
+    private func setupHeader() {
+        
+        header.addSubview(headerImageView)
+        headerImageView.topAnchor.constraint(equalTo: header.topAnchor, constant: -5).isActive = true
+        headerImageView.leadingAnchor.constraint(equalTo: header.leadingAnchor, constant: -6).isActive = true
+        headerImageView.heightAnchor.constraint(equalTo: header.heightAnchor).isActive = true
+        
+        header.addSubview(homeImageView)
+        homeImageView.topAnchor.constraint(equalTo: header.topAnchor, constant: 55).isActive = true
+        homeImageView.centerXAnchor.constraint(equalTo: header.centerXAnchor).isActive = true
+        
+        header.addSubview(menuButton)
+        menuButton.topAnchor.constraint(equalTo: header.topAnchor, constant: 50).isActive = true
+        menuButton.leadingAnchor.constraint(equalTo: header.leadingAnchor, constant: 15).isActive = true
+        
+        header.addSubview(bienvenido)
+        
+        bienvenido.bottomAnchor.constraint(equalTo: header.bottomAnchor, constant: -37).isActive = true
+        bienvenido.leadingAnchor.constraint(equalTo: header.leadingAnchor, constant: 15).isActive = true
+        
+        header.addSubview(bienvenidoSub)
+        bienvenidoSub.bottomAnchor.constraint(equalTo: header.bottomAnchor, constant: -20).isActive = true
+        bienvenidoSub.leadingAnchor.constraint(equalTo: header.leadingAnchor, constant: 15).isActive = true
+        
+        header.clipsToBounds = true
+        contentView.addArrangedSubview(header)
+        header.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/3).isActive = true
+    }
+    private func setupRecents() {
+        
+        recents.addSubview(archivosRecientes)
+        archivosRecientes.topAnchor.constraint(equalTo: recents.topAnchor, constant: 30).isActive = true
+        archivosRecientes.leadingAnchor.constraint(equalTo: recents.leadingAnchor, constant: 15).isActive = true
+        
+        recents.addSubview(loadNewFileButton)
+        loadNewFileButton.bottomAnchor.constraint(equalTo: recents.bottomAnchor, constant: -60).isActive = true
+        loadNewFileButton.leadingAnchor.constraint(equalTo: recents.leadingAnchor, constant: 15).isActive = true
+        loadNewFileButton.trailingAnchor.constraint(equalTo: recents.trailingAnchor, constant: -15).isActive = true
+        
+        contentView.addArrangedSubview(recents)
+        
+        recents.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 2/3).isActive = true
+        
+    }
+    
+    private func setupTableView() {
+        
+        
+        recents.addSubview(tableview)
+        
+        tableview.translatesAutoresizingMaskIntoConstraints = false
+        tableview.topAnchor.constraint(equalTo: archivosRecientes.bottomAnchor, constant: 25).isActive = true
+        tableview.leadingAnchor.constraint(equalTo: recents.leadingAnchor, constant: 15).isActive = true
+        tableview.trailingAnchor.constraint(equalTo: recents.trailingAnchor, constant: -15).isActive = true
+        tableview.bottomAnchor.constraint(equalTo: loadNewFileButton.topAnchor, constant: -25).isActive = true
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.register(CustomTableViewCell.self, forCellReuseIdentifier: "cell")
+        tableview.separatorStyle = .none
+    }
+    
+    @objc private func menu(sender: UIButton) {
+        print("Menu tapped")
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 78
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.selectionStyle = .none
+        cell.filename.text = fakeData[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Celda seleccionada")
+    }
+}
+
